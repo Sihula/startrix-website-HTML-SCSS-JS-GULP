699fd6bbb8918954446624d3543224db98cd35ed
Sihula <serhiisihula@gmail.com>
2021-11-25 19:46:23 +0100

    adaptive done. html/css/js/adaptive done

--- app/js/main.js
+++ app/js/main.js
@@ -4,16 +4,25 @@ let menu = document.querySelector('.header .menu');
 navbar.onclick = () => {
   navbar.classList.toggle('fa-times');
   menu.classList.toggle('active');
-}
+};
 
 
 $('.slider__inner').slick({
   infinite: true,
-  slidesToShow: 2
+  slidesToShow: 2,
+  responsive: [{
+    breakpoint: 1250,
+    settings: {
+      slidesToShow: 1,
+      dots: false,
+      fade: true,
+      autoplay: true
+    }
+  }]
 });
 
 document.querySelectorAll('.aacordion-container .accordion').forEach(accordion => {
   accordion.onclick = () => {
     accordion.classList.toggle('active');
-  }
-})
+  };
+});
