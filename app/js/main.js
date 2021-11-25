56888c5e41ab6291024309e135f920ceb6227f95
Sihula <serhiisihula@gmail.com>
2021-11-25 14:53:28 +0100

    end page

--- app/js/main.js
+++ app/js/main.js
@@ -5,3 +5,15 @@ navbar.onclick = () => {
   navbar.classList.toggle('fa-times');
   menu.classList.toggle('active');
 }
+
+
+$('.slider__inner').slick({
+  infinite: true,
+  slidesToShow: 2
+});
+
+document.querySelectorAll('.aacordion-container .accordion').forEach(accordion => {
+  accordion.onclick = () => {
+    accordion.classList.toggle('active');
+  }
+})
